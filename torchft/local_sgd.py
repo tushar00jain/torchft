d5098b8c0902d8817f45c36a5831cff22314afb5
Tushar Jain <tushar00jain@users.noreply.github.com>
2025-06-10 22:52:56 -0700

    integrate quantization in manager
    Summary:
    - add back support for quantized allreduce in manager
    - change return types to be consistent with pg allreduce

--- torchft/local_sgd.py
+++ torchft/local_sgd.py
@@ -209,7 +209,9 @@ class _StreamingDiLoCoFragment:
         self.use_bucketization = use_bucketization
         self.should_quantize = should_quantize
 
+        self._grads = {}
         self.original_parameters: Dict[str, torch.Tensor] = {}
+
         for name, p in self._model_fragment.named_parameters():
             if isinstance(p, DTensor):
                 p = extract_local_tensor(p.data)
@@ -249,18 +251,16 @@ class _StreamingDiLoCoFragment:
                         ),
                         non_blocking=False,
                     )
+                    # TODO: copy over grad
                 else:
                     p.data.copy_(self.original_parameters[name], non_blocking=False)
+                    p._grad.copy_(self._grads[name], non_blocking=False)
 
     @torch.profiler.record_function("torchft::local_sgd::wait")
     def wait(self) -> None:
         """
         Waits for the previously scheduled allreduce to finish
         """
-
-        for work in self._allreduce_futures:
-            work.wait()
-
         if self._stream is not None:
             self._stream.synchronize()
 
@@ -286,6 +286,10 @@ class _StreamingDiLoCoFragment:
         Calculate the pseugradient, average them across the manager group and starts
         allreduce on the pseudo-gradients but doesn't wait for it to finish.
         """
+        # Make sure tensors are available to `_stream`
+        if self._stream is not None:
+            self._stream.wait_stream(torch.cuda.current_stream())
+
         with (
             torch.cuda.stream(self._stream)
             if self._stream is not None
@@ -299,10 +303,14 @@ class _StreamingDiLoCoFragment:
                 )
                 if isinstance(p, DTensor):
                     p.grad._local_tensor = pseudogradient
+                    # TODO: use `self._grads`
                 else:
-                    p.grad = pseudogradient
+                    self._grads[name] = pseudogradient
 
             self._average_grads()
+
+            for work in self._allreduce_futures:
+                work.wait()
 
     @torch.profiler.record_function("torchft::local_sgd::perform_sync")
     def perform_sync(self) -> bool:
@@ -609,7 +617,6 @@ class DiLoCo:
         #
         # Both of them will fail because Node A didn't send fragment 2
         # and Node B didn't send fragment 1.
-        step = 0
         with self._lock:
             self._local_step += 1
             step = self._local_step
