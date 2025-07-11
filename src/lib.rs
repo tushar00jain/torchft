2210b6c1b4a633f067c0b8b59a5f2f08826f8990
Tushar Jain <tushar00jain@users.noreply.github.com>
2025-07-11 15:06:14 -0700

    retry quorum
    Summary:
    - we currently don't retry quorum requests from the manager to lighthouse
    - if lighthouse crashes, this can result in all replicas crashing
    - so add retries configurable through env var
    - remove timeout on the request made the the manager server

--- src/lib.rs
+++ src/lib.rs
@@ -71,6 +71,7 @@ fn num_threads() -> usize {
 ///     world_size (int): The world size of the replica group.
 ///     heartbeat_interval (timedelta): The interval at which heartbeats are sent.
 ///     connect_timeout (timedelta): The timeout for connecting to the lighthouse server.
+///     quorum_retries (int): The number of retries for quorum requests to lighthouse server.
 #[pyclass]
 struct ManagerServer {
     handle: JoinHandle<Result<()>>,
@@ -91,6 +92,7 @@ impl ManagerServer {
         world_size: u64,
         heartbeat_interval: Duration,
         connect_timeout: Duration,
+        quorum_retries: i64,
     ) -> PyResult<Self> {
         py.allow_threads(move || {
             let runtime = tokio::runtime::Builder::new_multi_thread()
@@ -108,6 +110,7 @@ impl ManagerServer {
                     world_size,
                     heartbeat_interval,
                     connect_timeout,
+                    quorum_retries
                 ))
                 .map_err(|e| PyRuntimeError::new_err(e.to_string()))?;
             let handle = runtime.spawn(manager.clone().run());
@@ -190,9 +193,11 @@ impl ManagerClient {
                 commit_failures: commit_failures,
             });
 
-            // This timeout is processed on the server side so we also enable
-            // keep alives to detect server health.
-            request.set_timeout(timeout);
+            // TODO: Have some mechanism to detect server health.
+            //
+            // We can't use timeouts here e.g. using `request.set_timeout(timeout)`
+            // because the server might need to retry quorum requests to the
+            // lighthouse server.
 
             let response = self.runtime.block_on(self.client.clone().quorum(request))?;
             let resp = response.into_inner();
