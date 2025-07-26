92ad240bb29f85aa23be3354876fd5ab2daf22d7
Tushar Jain <tushar00jain@users.noreply.github.com>
2025-07-25 21:29:27 -0700

    option 2 - call work.wait inside future callback

--- torchft/manager.py
+++ torchft/manager.py
@@ -397,12 +397,13 @@ class Manager:
             def callback(
                 fut: torch.futures.Future[List[torch.Tensor]],
             ) -> torch.Tensor:
-                nonlocal tensor, stream, num_participants
+                nonlocal tensor, stream, num_participants, work
 
                 # change the stream to avoid making the callback stream
                 # dependent on process group stream running the allreduce
                 with torch.cuda.stream(stream) if stream is not None else nullcontext():
                     # Setup stream dependency
+                    work.wait()
                     fut.wait()
                     fut.value()
                     tensor /= num_participants
