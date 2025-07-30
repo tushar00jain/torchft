4e7311a7aed8c0c4a133ab8e8cd410dbbb349956
Tushar Jain <tushar00jain@users.noreply.github.com>
2025-07-29 22:19:11 -0700

    make checkpointing thread safe
    Summary:
    - the regression tests fail (on future changes) because it expects no recovery to happen, or it happens at the first step
    - because we validate the parameters at each step, if recovery happens non deterministically, we can't really validate the parameters
    - to fix this, added local_step to the state dict because we recover the model from checkpoint
    - the checkpointing also wasn't thread safe for http transport so use lock the model in the pre step hook and when we want to transfer the checkpoint

--- torchft/local_sgd.py
+++ torchft/local_sgd.py
@@ -85,6 +85,9 @@ class LocalSGD:
         self._hooks: List[RemovableHandle] = []
 
     def __enter__(self) -> "LocalSGD":
+        self._hooks.append(
+            self._local_optimizer.register_step_pre_hook(self._step_pre_hook)
+        )
         # Add optimizer hook which increments the local step counter and syncs if necessary
         self._hooks.append(
             self._local_optimizer.register_step_post_hook(self._step_post_hook)
@@ -105,12 +108,20 @@ class LocalSGD:
 
         return False  # Propagate exceptions
 
+    def _step_pre_hook(
+        self, _optim: optim.Optimizer, _args: Tuple[Any, ...], _kwargs: Dict[str, Any]
+    ) -> None:
+        # The checkpoint may transfer model parameters, so we need to make access to it thread safe
+        self._manager.disallow_checkpoint()
+
     def _step_post_hook(
         self, _optim: optim.Optimizer, _args: Tuple[Any, ...], _kwargs: Dict[str, Any]
     ) -> None:
         """
         This hook is registered on the optimizer and is called after the optimizer step.
         """
+        self._manager.allow_checkpoint()
+
         self._local_step += 1
         if self._local_step >= self._sync_every:
             self.sync()
@@ -657,6 +668,16 @@ class DiLoCo:
     def _register_state_dict_fn(self) -> None:
         for fragment in self._fragments:
             fragment.register_state_dict_fn()
+
+        def load_local_step(local_step: int) -> None:
+            self._local_step = local_step
+
+        def save_local_step() -> int:
+            return self._local_step
+
+        self._manager.register_state_dict_fn(
+            "local_step", load_local_step, save_local_step
+        )
 
     def _save_parameters(self) -> None:
         for fragment in self._fragments:
