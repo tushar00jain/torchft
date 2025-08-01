d358fb4aaa9220cef7e4e68f1050ef8c2a815e0d
Tushar Jain <tushar00jain@users.noreply.github.com>
2025-08-01 12:17:02 -0700

    setup stream dependencies inside work wrapper (#248)
    Summary:
    - extend the work wrapper object to also do the division post allreduce
    - add api to block_current_stream on work wrapper so it can be used for HSDP

--- torchft/local_sgd.py
+++ torchft/local_sgd.py
@@ -524,6 +524,7 @@ class _StreamingDiLoCoFragment:
                             flat_buffer[pack_offset : pack_offset + numel].view_as(t)
                         )
 
+            work.synchronize()
             fut = work.get_future()
             fut.add_done_callback(callback)
 
