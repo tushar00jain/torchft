c74a1b03e032b184cb6ee89c751a83f91e3ae2fb
Tushar Jain <tushar00jain@users.noreply.github.com>
2025-09-24 14:26:55 -0400

    fix managed pg allreduce (#249)
    Summary:
    managed pg allreduce should just call manager's allreduce
    
    Co-authored-by: Tristan Rice <rice@fn.lc>

--- torchft/manager.py
+++ torchft/manager.py
@@ -381,6 +381,7 @@ class Manager:
         self,
         tensor: torch.Tensor,
         should_quantize: bool = False,
+        reduce_op: ReduceOp = ReduceOp.SUM,
     ) -> Work:
         """
         Fault tolerant allreduce the tensor and return a Future that will be completed when
@@ -416,13 +417,13 @@ class Manager:
             if should_quantize and IS_TRITON_AVAILABLE:
                 work = allreduce_quantized(
                     [tensor],
-                    ReduceOp.SUM,
+                    reduce_op,
                     self._pg,
                     # pyre-fixme[6]: Expected `Optional[streams.Stream]` but got `_C.Stream`
                     torch.accelerator.current_stream(),
                 )
             else:
-                work = self._pg.allreduce([tensor], ReduceOp.SUM)
+                work = self._pg.allreduce([tensor], reduce_op)
 
             # schedule grad normalization as a continuation
             # on the Future
@@ -431,7 +432,8 @@ class Manager:
                 fut: torch.futures.Future[list[torch.Tensor]],
             ) -> torch.Tensor:
                 nonlocal tensor
-                tensor /= num_participants
+                if reduce_op == ReduceOp.SUM:
+                    tensor /= num_participants
                 return tensor
 
             managed_work = _ManagedWork(self, work, tensor)
