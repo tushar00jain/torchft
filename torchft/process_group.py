647979bb8b5f54f56bdfd2c705779c7fdf24c78d
Tushar Jain <tushar00jain@users.noreply.github.com>
2025-09-25 11:11:24 -0700

    use global rank for flight recorder

--- torchft/process_group.py
+++ torchft/process_group.py
@@ -738,7 +738,6 @@ class ProcessGroupNCCL(ProcessGroupWrapper):
         self._use_abort: bool = torch.cuda.nccl.version() >= (2, 25)
 
         self._errored: Optional[Exception] = None
-        self._rank: int = 0
 
         NONBLOCKING_TIMEOUT_ENV = "TORCH_NCCL_NONBLOCKING_TIMEOUT"
         if NONBLOCKING_TIMEOUT_ENV not in os.environ:
@@ -788,7 +787,6 @@ class ProcessGroupNCCL(ProcessGroupWrapper):
         from torch.distributed import ProcessGroupNCCL as BaseProcessGroupNCCL
 
         self._errored = None
-        self._rank = rank
 
         # pyre-fixme[16]: no attribute ProcessGroupNCCL
         opts = BaseProcessGroupNCCL.Options()
@@ -811,8 +809,8 @@ class ProcessGroupNCCL(ProcessGroupWrapper):
         # We need to set the error before aborting to ensure that errored()
         # returns the error correctly when NCCL abort fires and unblocks the
         # stream.
-        if os.environ.get("TORCHFT_TRIGGER_FR_ON_ABORT", "true") == "true":
-            trigger_nccl_fr_trace_through_pipe(self._rank)
+        if os.environ.get("TORCHFT_TRIGGER_FR_ON_ABORT", "false") == "true":
+            trigger_nccl_fr_trace_through_pipe(dist.get_rank())
         self._errored = RuntimeError("aborted")
 
         super().abort()
